b0643f20814e249295f46a385186488c92e1428b
Emmanuel Paul Carrillo Carpio <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-03-29 18:39:50 -0500

    config

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -10,7 +10,7 @@ import { UserRolesRouter } from "./modules/UserRoles/UserRoles.route";
 class App {
   private app = express();
   private PORT = process.env.PORT || 3000;
-  private router = new UserRolesRouter();
+
   
   
 
@@ -24,7 +24,7 @@ class App {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cors());
 
-    this.app.use("/api", this.router.routes);
+    this.app.use("/api", new UserRolesRouter().routes);
 
     this.app.listen(this.PORT, () => {
       console.log(`Server running on port ${this.PORT}`);
