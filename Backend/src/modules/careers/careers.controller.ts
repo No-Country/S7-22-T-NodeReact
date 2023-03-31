a615c72eb7559e7a13540528f6e0f5ff33251aaf
Emmanuel Paul Carrillo Carpio <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-03-31 00:44:19 -0500

    config_careers_with_routes_services_and_controller

---
+++ Backend/src/modules/careers/careers.controller.ts
@@ -0,0 +1,89 @@
+import { Request, Response } from "express";
+import { CareersEntity } from "./careers.entity";
+import { CareersServices } from "./careers.services";
+
+export class CareersController extends CareersServices {
+    constructor() {
+        super();
+    }
+
+    async getCareers(req: Request, res: Response) {
+        try {
+            const careers = await this.getCareerService()
+            res.status(200).json({
+                status: true,
+                careers
+            })
+        } catch (error) {
+            res.status(500).json({ msg: error });
+        }
+    }
+
+    async getCareerById(req: Request, res: Response) {
+        const { id } = req.params
+        try {
+            const career = await this.getCareerByIdService(Number(id));
+            if (!career)
+                return res.status(404).json({
+                    status: false,
+                    msg: "Career not found"
+                })
+
+            res.status(200).json({
+                status: true,
+                career
+            })
+        } catch (error) {
+            res.status(500).json({ msg: error })
+        }
+    }
+
+    async postCareer(req: Request, res: Response) {
+        const body: CareersEntity = req.body;
+        try {
+            const career = await this.postCareerService(body);
+            res.status(200).json({
+                status: true,
+                career
+            })
+        } catch (error) {
+            res.status(500).json({ msg: error })
+        }
+    }
+
+    async putCareer(req: Request, res: Response) {
+        const { id } = req.params;
+        const body: CareersEntity = req.body;
+        try {
+            const career = await this.getCareerByIdService(Number(id))
+            if (!career)
+                return res.status(404).json({
+                    status: false,
+                    msg: "School not found"
+                })
+
+            await this.putCareerService(Number(id), body)
+            const career2 = await this.getCareerByIdService(Number(id))
+            res.status(200).json({
+                status: true,
+                career2,
+            })
+        } catch (error) {
+            res.status(500).json({ msg: error });
+        }
+    }
+
+    async deleteCareer(req: Request, res: Response) {
+        const { id } = req.params
+        try {
+            const career = await this.getCareerByIdService(Number(id))
+            await this.deleteCareerService(Number(id))
+            res.status(200).json({
+                status: true,
+                career
+            })
+        } catch (error) {
+            res.status(500).json({ msg: error })
+        }
+    }
+}
---
+++ Backend/src/modules/careers/interfaces/careers.interfaces.ts
@@ -0,0 +1,3 @@
+export interface Careers {
+    careerName:string
+}
