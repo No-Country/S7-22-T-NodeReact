5d1e08c75ad25099d80727c73a22716353d87e59
Emmanuel Paul Carrillo Carpio <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-04-07 13:37:50 -0500

    config_mod_utils

--- Backend/src/modules/user/utils/passwordEncrypt.ts
+++ Backend/src/modules/user/utils/passwordEncrypt.ts
@@ -4,6 +4,7 @@ export const hashPassword = (pass: string) => {
   return bcrypt.hash(pass, 10);
 };
 
-export const checkPassword = (pass: string, hashPass: string) => {
-  return bcrypt.compare(pass, hashPass);
+export const checkPassword = async (pass: string, hashPass: string) => {
+  const isCorrect = await bcrypt.compare(pass, hashPass);
+  return isCorrect;
 };
