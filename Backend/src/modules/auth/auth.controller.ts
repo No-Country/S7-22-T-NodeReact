82f0397e308a6b9f40f0262c21a0801eec16b524
Emmanuel Paul Carrillo Carpio <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-04-07 13:37:00 -0500

    auth_configuration

--- Backend/src/modules/auth/auth.controller.ts
+++ Backend/src/modules/auth/auth.controller.ts
@@ -3,81 +3,114 @@ import { Request, Response } from "express";
 import { AuthEntity } from "./auth.entity";
 import { AuthServices } from "./auth.services";
 import { randomUUID } from "crypto";
+import { UserEntity } from "../user/user.entity";
+import { hashPassword, checkPassword } from "../user/utils/passwordEncrypt";
+import { generateToken, verifyToken } from "../user/utils/jwtHandle";
+import { BaseServices } from "../../shared/services/baseServices";
 
 export class AuthController extends AuthServices {
   constructor() {
     super();
   }
 
-  async getAll(req: Request, res: Response) {
-    try {
-      const Auths = await this.getServices();
 
-      res.status(200).json({
-        status: true,
-        Auths,
-      });
+  async authLoginUser(req: Request, res: Response) {
+    try {
+      const { email, password }: AuthEntity = req.body;
+      const authExistUser = await UserEntity.findOne({ where: { email } });
+      if (!authExistUser) {
+        return res.status(401).json({ error: "User not exist" });
+      }
+      const authExistPassword = authExistUser.password;
+      const isCorrectPass = await checkPassword(password, authExistPassword);
+    
+      if(isCorrectPass){
+        const token = generateToken(authExistUser.email);
+        const data = {
+          token,
+          user: authExistUser
+        };
+        return res.json(data);
+      }
+
+      if (!isCorrectPass) {
+        return res.status(401).json({ error: "Incorrect password" });
+      }
     } catch (error) {
       res.status(500).json({ msg: error });
     }
   }
 
-  async getById(req: Request, res: Response) {
-    const { id } = req.params;
-    try {
-      const Auth = await this.getServicesById(Number(id));
 
-      res.status(200).json({
-        status: true,
-        Auth,
-      });
-    } catch (error) {
-      res.status(500).json({ msg: error });
-    }
-  }
+  // async getAll(req: Request, res: Response) {
+  //   try {
+  //     const Auths = await this.getServices();
 
-  async post(req: Request, res: Response) {
-    const body: AuthEntity = req.body;
-    try {
-      const Auth = await this.postService(body);
+  //     res.status(200).json({
+  //       status: true,
+  //       Auths,
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({ msg: error });
+  //   }
+  // }
 
-      res.status(200).json({
-        status: true,
-        Auth,
-      });
-    } catch (error) {
-      res.status(500).json({ msg: error });
-    }
-  }
+  // async getById(req: Request, res: Response) {
+  //   const { id } = req.params;
+  //   try {
+  //     const Auth = await this.getServicesById(Number(id));
 
-  async put(req: Request, res: Response) {
-    const { id } = req.params;
-    const body = req.body;
-    try {
-      await this.putService(Number(id), body);
-      const AuthUpdate = await this.getServicesById(Number(id));
+  //     res.status(200).json({
+  //       status: true,
+  //       Auth,
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({ msg: error });
+  //   }
+  // }
 
-      res.status(200).json({
-        status: true,
-        AuthUpdate,
-      });
-    } catch (error) {
-      res.status(500).json({ msg: error });
-    }
-  }
+  // async post(req: Request, res: Response) {
+  //   const body: AuthEntity = req.body;
+  //   try {
+  //     const Auth = await this.postService(body);
 
-  async delete(req: Request, res: Response) {
-    const { id } = req.params;
-    try {
-      const Auth = await this.getServicesById(Number(id));
+  //     res.status(200).json({
+  //       status: true,
+  //       Auth,
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({ msg: error });
+  //   }
+  // }
 
-      await this.deleteService(Number(id));
-      res.status(200).json({
-        status: true,
-        Auth,
-      });
-    } catch (error) {
-      res.status(500).json({ msg: error });
-    }
-  }
+  // async put(req: Request, res: Response) {
+  //   const { id } = req.params;
+  //   const body = req.body;
+  //   try {
+  //     await this.putService(Number(id), body);
+  //     const AuthUpdate = await this.getServicesById(Number(id));
+
+  //     res.status(200).json({
+  //       status: true,
+  //       AuthUpdate,
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({ msg: error });
+  //   }
+  // }
+
+  // async delete(req: Request, res: Response) {
+  //   const { id } = req.params;
+  //   try {
+  //     const Auth = await this.getServicesById(Number(id));
+
+  //     await this.deleteService(Number(id));
+  //     res.status(200).json({
+  //       status: true,
+  //       Auth,
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({ msg: error });
+  //   }
+  // }
 }
--- Backend/src/modules/auth/auth.middlewares.ts
+++ Backend/src/modules/auth/auth.middlewares.ts
@@ -1,9 +1,17 @@
 import { AuthEntity } from "./auth.entity";
 import { BaseMiddlewares } from "../../shared/middleware/baseMiddleware";
+import jwt, { GetPublicKeyOrSecret, Secret } from "jsonwebtoken";
+import { Response,Request,NextFunction  } from "express";
+import { UserEntity } from "../user/user.entity";
+import { FindOptionsWhere } from "typeorm";
+
+
 
 export class AuthMiddlewares extends BaseMiddlewares<AuthEntity> {
+  public secretKey = process.env.JWT_SECRET as Secret | GetPublicKeyOrSecret;
   constructor() {
     super(AuthEntity);
   }
 
+ 
 }
