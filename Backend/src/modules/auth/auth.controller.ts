b1221bd0b4b8233a556275e8b4b693d71a1a3b55
SortOmega <sortocarlo755@gmail.com>
2023-04-07 21:51:13 -0600

    Fixed authLoginUser controller

--- Backend/src/modules/auth/auth.controller.ts
+++ Backend/src/modules/auth/auth.controller.ts
@@ -3,7 +3,8 @@ import { Request, Response } from "express";
 import { AuthEntity } from "./auth.entity";
 import { AuthServices } from "./auth.services";
 import { generateToken } from "./utils/jwtHandle.utils";
-
+import { checkPassword } from "./utils/checkPass.utils";
+import { AuthResponses } from "./utils/auth.constants";
 export class AuthController extends AuthServices {
   constructor() {
     super();
@@ -11,10 +12,19 @@ export class AuthController extends AuthServices {
 
   async authLoginUser(req: Request, res: Response) {
     try {
-      const { email }: AuthEntity = req.body;
-      const user = await this.repository.findOne({ where: { email } });
+      const { email, password }: AuthEntity = req.body;
+      const userFound = await this.repository.findOne({ where: { email } });
 
+      if (userFound === null) return res.status(400).json(AuthResponses.errors.credential);
+
+      const userPassword = userFound?.password;
+      const isCorrectPass = await checkPassword(password, userPassword);
+
+      if (!isCorrectPass) return res.status(400).json(AuthResponses.errors.badPassword);
       const token = generateToken(email);
+
+      // sprite, excluding id from response
+      const { id, ...user } = userFound;
 
       return res.status(200).json({
         status: true,
---
+++ Backend/src/modules/auth/utils/auth.constants.ts
@@ -0,0 +1,6 @@
+export const AuthResponses = {
+  errors: {
+    credential: { status: false, msg: "Error while authenticating, no account has been found!" },
+    badPassword: { status: false, msg: "Your password does not match with your account!" },
+  },
+};
