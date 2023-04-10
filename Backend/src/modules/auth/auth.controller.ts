b18e9675c8cd3aa6041e11333603ece469cbefa5
Luis Mera <luismera86@gmail.com>
2023-04-10 18:44:09 -0300

    Fix checkLogin ans refactor authLogin

--- Backend/src/modules/auth/auth.controller.ts
+++ Backend/src/modules/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 
 import { AuthEntity } from "./auth.entity";
-import { AuthServices } from "./auth.services";
-import { generateToken } from "./utils/jwtHandle.utils";
-import { checkPassword } from "./utils/checkPass.utils";
 import { AuthResponses } from "./utils/auth.constants";
+import { AuthServices } from "./auth.services";
+import { UserDto } from "../user/user.dto";
+import { UserEntity } from "../user/user.entity";
+import { checkPassword } from "./utils/checkPass.utils";
+import { generateToken } from "./utils/jwtHandle.utils";
+
 export class AuthController extends AuthServices {
   constructor() {
     super();
@@ -12,18 +15,11 @@ export class AuthController extends AuthServices {
 
   async authLoginUser(req: Request, res: Response) {
     try {
-      const { email, password }: AuthEntity = req.body;
+      const { email }: AuthEntity = req.body;
       const userFound = await this.repository.findOne({ where: { email } });
 
-      if (userFound === null) return res.status(400).json(AuthResponses.errors.credential);
+      const { id, ...user } = userFound as UserEntity;
 
-      const userPassword = userFound?.password;
-      const isCorrectPass = await checkPassword(password, userPassword);
-
-      if (!isCorrectPass) return res.status(400).json(AuthResponses.errors.badPassword);
-
-      // sprite, excluding id from response
-      const { id, ...user } = userFound;
       const token = generateToken(user);
 
       return res.status(200).json({
--- Backend/src/modules/auth/auth.middlewares.ts
+++ Backend/src/modules/auth/auth.middlewares.ts
@@ -2,6 +2,7 @@ import { GetPublicKeyOrSecret, Secret } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 
 import { AuthEntity } from "./auth.entity";
+import { AuthResponses } from "./utils/auth.constants";
 import { BaseMiddlewares } from "../../shared/middleware/baseMiddleware";
 import { UserEntity } from "../user/user.entity";
 import { checkPassword } from "./utils/checkPass.utils";
@@ -9,30 +10,22 @@ import { checkPassword } from "./utils/checkPass.utils";
 export class AuthMiddlewares extends BaseMiddlewares<UserEntity> {
   public secretKey = process.env.JWT_SECRET as Secret | GetPublicKeyOrSecret;
   constructor() {
-    super(AuthEntity);
+    super(UserEntity);
   }
 
-  async checkLogin(req: Request, res: Response, nex: NextFunction) {
-    const { email, password }: AuthEntity = req.body;
+  async checkLogin(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
     try {
       const authExistUser = await this.repository.findOne({ where: { email } });
 
-      if (!authExistUser)
-        return res.status(401).json({
-          status: false,
-          response: "User mail not exist",
-        });
+      if (!authExistUser) return res.status(400).json(AuthResponses.errors.credential);
 
       const authExistPassword = authExistUser.password;
+
       const isCorrectPass = await checkPassword(password, authExistPassword);
 
-      if (!isCorrectPass)
-        res.status(401).json({
-          status: false,
-          response: "Password not found",
-        });
-
-      nex();
+      if (!isCorrectPass) return res.status(400).json(AuthResponses.errors.badPassword);
+      next();
     } catch (error) {
       res.status(500).json({ msg: error });
     }
