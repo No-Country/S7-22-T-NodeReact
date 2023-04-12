1b33a851b1de09896e2d8c4871227583a93f1c49
joacoguzmanz <joaquingusman77@gmail.com>
2023-04-12 19:38:30 -0300

    done index and career route

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,6 +1,6 @@
 const Menu = ({ children, isOpen }) => {
     return (
-        <nav className={`${isOpen ? 'absolute left-5 right-5 top-16' : 'hidden'} md:hidden`}>
+        <nav className={`${isOpen ? 'absolute z-50 left-5 right-5 top-16' : 'hidden'} md:hidden`}>
             <ul className='flex flex-col items-center gap-2.5 bg-primary w-full p-5 rounded-lg'>
                 { children }
             </ul>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,27 +1,11 @@
-import Sidebar from "@/components/sidebar/Sidebar";
-import Button from "@/components/Button";
-import InputField from "@/components/InputField";
 import Layout from "@/components/Layout";
-import { useState } from "react";
 
 export default function Home() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(name, email)
-    }
-
   return (
-      <>
-          <Layout name={'Inicio'}>
-              <form>
-                  <InputField label={'Name'} type={'text'} value={name} onChange={setName} />
-                  <InputField label={'Email'} type={'email'} value={email} onChange={setEmail} />
-                  <Button type={'submit'} text={'Registrame'} onClick={handleSubmit} visible={true} />
-              </form>
-          </Layout>
-      </>
+      <Layout name={'Inicio'}>
+          <div className='h-screen'>
+              <h1>Inicio</h1>
+          </div>
+      </Layout>
   )
 }
