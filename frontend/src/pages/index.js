f302c5a357bd31fa45ef1f76b0e72c319bf1cb23
Emanuel Manrique <manriqueemanuel@gmail.com>
2023-04-12 20:35:02 -0300

    test

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,14 +1,11 @@
-import Sidebar from "@/components/sidebar/Sidebar";
-import Subjects from "@/components/subjects/Subjects";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
-    <div>
-    
-    <Subjects />    
-    {/* // <h1 className="text-3xl font-bold underline">
-    //   Hello world!
-    // </h1> */}
-    </div>
+      <Layout name={'Inicio'}>
+          <div className='h-screen'>
+              <h1>Inicio</h1>
+          </div>
+      </Layout>
   )
 }
