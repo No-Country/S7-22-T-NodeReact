903f7225f169b99193c531bb9161316fdbe68774
SortOmega <sortocarlo755@gmail.com>
2023-04-17 17:07:14 -0600

    Changes to connection config

--- Backend/src/config/db/postgreSql.ts
+++ Backend/src/config/db/postgreSql.ts
@@ -15,10 +15,18 @@ import { ActivityTypeEntity } from "../../modules/activityType/activityType.enti
 import { CommissionsActivitiesEntity } from "../../modules/commissionActivities/commissionActivities.entity";
 import { StudentActivitiesEntity } from "../../modules/studentActivities/studentActivities.entity";
 
+const DBPort = !process.env.PORT_DB ? 5432 : parseInt(process.env.PORT_DB);
+
+const setSSL = !process.env.SSL_SUPPORT
+  ? false
+  : process.env.SSL_SUPPORT === "0"
+  ? false
+  : process.env.SSL_SUPPORT === "1" && true;
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.HOST_DB,
-  port: 5432,
+  port: DBPort,
   username: process.env.USERNAME_DB,
   password: process.env.PASS_DB,
   database: process.env.NAME_DB,
@@ -38,9 +46,9 @@ export const AppDataSource = new DataSource({
     ClassesEntity,
     ActivityTypeEntity,
     CommissionsActivitiesEntity,
-    StudentActivitiesEntity
+    StudentActivitiesEntity,
   ],
   subscribers: [],
   migrations: [],
-  ssl:true
+  ssl: setSSL,
 });
